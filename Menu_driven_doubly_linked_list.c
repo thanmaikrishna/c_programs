e252aea3cd5a93fbc68ca7c2938dcbd8ab2c392d
Thanmai <152688241+thanmaikrishna@users.noreply.github.com>
2024-02-27 23:06:59 +0530

    Menu_driven_doubly_linked_list.c

--- Menu_driven_doubly_linked_list.c
+++ Menu_driven_doubly_linked_list.c
@@ -7,3 +7,13 @@ struct Node *next;
 };
 typedef struct Node node;
 node *start=NULL;
+node *getnode(){
+}
+void insert_start(int);
+void insert_after(int,int);
+void insert_end(int);
+void delete_start();
+void delete_after(int);
+void delete_last();
+void traverse();
+void search(int);
